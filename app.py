976f99b145da83025f6f2a05d788cb085838671e
arpeterson93 <arpeterson93@gmail.com>
2026-05-29 00:19:15 -0500

    Log AB UI changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ pages = [
     st.Page("pages/log_ab.py", title="Log AB", icon="📝"),
     st.Page("pages/1_Sessions.py", title="Sessions", icon="📅"),
     st.Page("pages/2_Pitcher_Scouting.py", title="Pitcher Scouting", icon="⚾"),
-    st.Page("pages/3_Batter_Scouting.py", title="Batter Scouting", icon="🏏"),
+    st.Page("pages/3_Batter_Scouting.py", title="Batter Scouting", icon="🦇"),
 ]
 pg = st.navigation(pages)
 pg.run()
